bebbb2fbc4608680e87acfa221b3e74f9a5289a6
llamylaliberte <124820227+llamylaliberte@users.noreply.github.com>
2024-01-15 14:51:53 -0500

    Update run.jl
    optimizer choice

--- run.jl
+++ run.jl
@@ -1,6 +1,6 @@
 
 using AnyMOD
-using Gurobi # other JuMP compatible solvers should work as well
+using HiGHS # other JuMP compatible solvers should work as well
 
 # enter one of the tree cases provided for the benchmark problem ("power_hourly","power_and_heat_hourly","all_hourly")
 case = "power_hourly"
